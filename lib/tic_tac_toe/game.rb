d6a12a80642345fd722d739368c407b79ac9374b
Michael Marchand <michaelmarchand@koobcam.local>
2019-09-06 14:28:26 -0600

    added upto #play method, with working tests

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -15,6 +15,39 @@
     def solicit_move
       "#{current_player.name}: select a number from 1 - 9 to make a move"
     end
+
+    def get_move(human_move = gets.chomp)
+      human_move_to_coordinate(human_move)
+    end
+
+    def game_over_message
+      return "#{current_player.name} won!" if board.game_over == :winner
+      return "the game ended in a draw" if board.game_over == :draw
+    end
+
+    def play
+      #cool stuff here
+      puts solicit_move
+      
+    end
+
+    private
+
+    def human_move_to_coordinate(human_move)
+      mapping = {
+        "1" => [0,0],
+        "2" => [1,0],
+        "3" => [2,0],
+        "4" => [0,1],
+        "5" => [1,1],
+        "6" => [2,1],
+        "7" => [0,2],
+        "8" => [1,2],
+        "9" => [2,2]
+      }
+      mapping[human_move]
+    end
+
   end
     
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -39,11 +39,39 @@ module TicTacToe
 
     context "#solicit_move" do
       it "sends a message to the user" do
-        allow_any_instance_of(Array).to receive(:shuffle) { [frank, bob] }
-        game = Game.new([bob, frank])
+        game = Game.new([frank, bob])
+        allow(game).to receive(:current_player) { frank }
         expect(game.solicit_move).to eq("frank: select a number from 1 - 9 to make a move")
       end
     end
+
+    context "#get_move" do
+      it "converts human move of 1 to [0,0]" do
+        game = Game.new([bob, frank])
+        expect(game.get_move("1")).to eq([0,0])
+      end
+      it "converts human move of 7 to [0,2]" do
+        game = Game.new([bob, frank])
+        expect(game.get_move("7")).to eq([0,2])
+      end
+    end
+
+    context "#game_over_message" do
+      it "returns the name of the winner" do
+        game = Game.new([frank, bob])
+        allow(game).to receive(:current_player) { frank }
+        allow(game.board).to receive(:game_over) { :winner }
+        expect(game.game_over_message).to eq("frank won!") 
+      end
+
+      it "returns 'the game ended in a draw' if no winner" do
+        game = Game.new([frank, bob])
+        allow(game.board).to receive(:game_over) { :draw }
+        expect(game.game_over_message).to eq("the game ended in a draw") 
+      end
+    end
+
+
 
   end
 end
